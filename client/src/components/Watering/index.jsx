66f4f3d2b8a4749a020dbda5066da5660fd71044
Tran Bao <bao.tran133@hcmut.edu.vn>
2021-05-16 06:43:19 +0700

    update

--- client/src/components/Watering/index.jsx
+++ client/src/components/Watering/index.jsx
@@ -1,6 +1,8 @@
 import styled from "styled-components"
 import Header from "../Header"
-import { getFader } from "../../utils/color"
+import React, { useState, useEffect } from 'react';
+import { getFader } from "../../utils/color";
+
 
 
 const Container = styled.div`
@@ -19,28 +21,11 @@ const Body = styled.div`
     gap: 1rem;
     
 `
-
-const Button = styled.button`
-    display: block;    
-    width:100%;    
-    background: ${props => props.theme.color.fill.primary};
-    color: ${props => props.theme.color.background.primary};
-    border: none;
-    outline: none;
-    padding: 0.5rem 0.5rem;
-    border-radius: 0.5rem;
-    gap: 0.5rem;
-    &:hover {
-        background: ${props => getFader(props.theme.color.fill.primary, 0.8)}
-    }
-`
-
-
 const Wrapper = styled.div`
     
     display:flex;    
     flex-direction: column;
-    gap: 0.5rem;
+    gap: 0.7rem;
     padding: 0.2rem;
     align-items:center;
     height:20%;
@@ -65,25 +50,96 @@ const Wrapper = styled.div`
         font-size: large;
     }
 `
-
+const Button = styled.button`
+    display: block;    
+    width:100%;    
+    background: ${props => props.theme.color.fill.primary};
+    color: ${props => props.theme.color.background.primary};
+    border: none;
+    outline: none;
+    padding: 0.5em 0.5em;
+    border-radius: 0.5rem;    
+    font-size: large;
+    &:hover {
+        background: ${props => getFader(props.theme.color.fill.primary, 0.8)}
+    }
+`
+const ProgessBar = styled.div`
+    border-radius: 0.2rem;    
+    display:flex;
+    heigh:100%;
+    width: 100%;    
+    justify-content: center;  
+    flex-direction:row;
+    border: 1px solid ${props => getFader(props.theme.color.fill.primary, 0.4)};
+`
+const Progess = styled.div`
+    display:flex;    
+    background: ${props => getFader(props.theme.color.fill.primary, 0.4)};       
+    border-radius: 0.5rem;
+    outline: none;    
+    padding: 0.45em;
+    transition: border 0.15s ease-in-out;    s    
+    flex-direction: row;
+    heigh:100%;
+    width: ${props => props.x*100}%;
+    max-width: 100%;    
+    justify-content: center;     
+    transition: width 800ms linear;
+   
+`
 
 
 
 const Watering = () => {
+    const [complete, setComplete] = useState(true);   
+    const [duration, setDuration] = useState(0);
+    const [timeLeft, setTimeLeft] = useState(duration);    
+    const [progess, setProgess] = useState(0);
+    
+    useEffect(() => {
+        
+        if (timeLeft < 0) {   
+            
+            setComplete(true);
+            return;
+        }
+        const intervalId = setInterval(() => {
+            setTimeLeft(timeLeft - 1);
+            setProgess(progess => parseInt(progess) + 1 - timeLeft / duration);
+            
+        }, 1000);
+        return () => clearInterval(intervalId);
+
+    }, [ timeLeft, progess, duration ]);
+
     return (
         <Container>
             <Header text={'Tưới cây thủ công'} />
-            <Body>     
-                                           
-                <Wrapper>
-
-                    <input type='text' placeholder="Nhập thời gian tưới..."/>                   
-                
-                
-                    <Button >   Bắt đầu   </Button>
-                    
-                </Wrapper>
-
+            <Body>
+                {complete ?
+                    <Wrapper>
+                        <input type='text' name = 'Bat dau' onChange={(e) => setDuration(parseInt(e.target.value))}  placeholder="Nhập thời gian tưới..." />
+                        <Button onClick={(e) => {
+                            if (duration !== 0) {
+                                setTimeLeft(duration);
+                                setProgess(0);
+                                setComplete(false);
+                            } else {
+                                window.location.reload()
+                            }                            
+                        }} >
+                            Bắt đầu
+                        </Button>
+                    </Wrapper>
+                    :
+                    <Wrapper>
+                        <ProgessBar>                            
+                            <Progess x={progess}><h2>{timeLeft}</h2></Progess>                            
+                        </ProgessBar>
+                        <Button onClick={() => window.location.reload()} > Ngừng   </Button>
+                    </Wrapper>
+                }
             </Body>
         </Container>
     )
