09c6c88fae50f5b8ddb3d759483f2c651ee608dd
Tran Bao <bao.tran133@hcmut.edu.vn>
2021-05-16 17:32:32 +0700

    update

--- client/src/components/History/index.jsx
+++ client/src/components/History/index.jsx
@@ -1,10 +1,15 @@
 import styled from "styled-components"
 import Header from "../Header"
+import React from 'react'
+import ReactDataGrid from '@inovua/reactdatagrid-enterprise';
+import '@inovua/reactdatagrid-enterprise/index.css';
+
 
 const Container = styled.div`
     display: flex;
     flex-direction: column;
     height: 100%;
+    
 `
 const Body = styled.div`
     flex: 1;
@@ -12,14 +17,45 @@ const Body = styled.div`
     display: flex;
     flex-direction: column;
     gap: 1rem;
+    background: ${props => props.theme.color.background.primary};
 `
 
+const columns = [
+    { name: 'time', header: 'Thời điểm', defaultFlex: 2 },
+    { name: 'humid', header: 'Độ ẩm', defaultFlex: 1  },
+    { name: 'duration', header: 'Thời gian \n (giây)', defaultFlex: 1 }
+];
+const gridStyle = { minHeight: 550 };
+const rows = [
+    { time: '16/5/2021 - 15:00', humid: '50', duration: '300' },
+    { time: '16/5/2021 - 15:00', humid: '50', duration: '300' },
+    { time: '16/5/2021 - 15:00', humid: '50', duration: '300' },
+    { time: '16/5/2021 - 15:00', humid: '50', duration: '300' },
+    { time: '16/5/2021 - 15:00', humid: '50', duration: '300' },
+    { time: '16/5/2021 - 15:00', humid: '50', duration: '300' },
+    { time: '16/5/2021 - 15:00', humid: '50', duration: '300' },
+    { time: '16/5/2021 - 15:00', humid: '50', duration: '300' },
+    { time: '16/5/2021 - 15:00', humid: '50', duration: '300' },
+    { time: '16/5/2021 - 15:00', humid: '50', duration: '300' },
+    { time: '16/5/2021 - 15:00', humid: '50', duration: '300' }
+
+];
+
+
 const History = () => {
-    return (
+    
+
+    
+    return (       
         <Container>
             <Header text={'Xem lịch sử tưới'}/>
             <Body>
-                Xem lịch sử tưới
+                <ReactDataGrid 
+                    idProperty="time"
+                    columns={columns}
+                    dataSource={rows}                    
+                    style={gridStyle}
+                    />
             </Body>
         </Container>
     )
