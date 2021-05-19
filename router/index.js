5488da85b6a9b193ddc268af387796c0b191a339
La Quoc Anh <anh.lq@trungthuy.vn>
2021-05-19 08:04:14 +0700

    route restructuring

---
+++ router/index.js
@@ -0,0 +1,7 @@
+let settingRoute = require('./settingRoute')
+let modeRoute = require('./modeRoute')
+let powerRoute = require('./powerRoute')
+let authRoute = require('./authRoute')
+let humidRoute = require('./humidRoute')
+
+module.exports = {settingRoute, modeRoute, powerRoute, authRoute, humidRoute}
--- server.js
+++ server.js
@@ -5,17 +5,12 @@ require('dotenv').config()
 
 let {adafruit} = require('./connection')
 let {requireAuth} = require('./middlewares/authMiddleware')
-let settingRoute = require('./router/settingRoute')
-let modeRoute = require('./router/modeRoute')
-let powerRoute = require('./router/powerRoute')
-let authRoute = require('./router/authRoute')
-let humidRoute = require('./router/humidRoute')
+let {settingRoute, modeRoute, powerRoute, authRoute, humidRoute} = require('./router')
 let {handleIotButton} = require('./iotFunctions')
 
 const app = express()
 app.use(express.json())
 app.use(cookieParser())
-
 
 app.use('/api/auth', authRoute)
 app.use('/api/setting',requireAuth, settingRoute)
