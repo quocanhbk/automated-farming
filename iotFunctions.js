84b4220dfc74a8cdb632cdce44c2a5dd6126b188
La Quoc Anh <anh.lq@trungthuy.vn>
2021-05-22 16:47:18 +0700

    fix buzzer

--- iotFunctions.js
+++ iotFunctions.js
@@ -1,5 +1,4 @@
-let express = require('express')
-let router = express.Router();
+const feedList = require('./feedList')
 const {dbConn,adafruit} = require('./connection')
 
 
@@ -35,62 +34,61 @@ module.exports.handleIotButton = (message) => {
 }
 
 // gửi thông báo buzzer
-    // let q = `SELECT sstatus FROM mainsystem WHERE id = 101`;
-    // dbConn.query(q,function(err, result){
-    //     if (err) res.json({error: err})
-    //     else { 
-    //         let power = result[0]["sstatus"] == 1 ? "on":"off"
-    //         if(power = "on"){
-    //             let message3 = {
-    //                 id: "3",
-    //                 name: "SPEAKER",
-    //                 data: 1000,
-    //                 unit:""
-    //             }
-    //             setTimeout(function () {
-    //             adafruit.publish(feed3.link, message3);
-    //             }, 1000);}
-    //         }
-    //     }
-    // )
+module.export = function runBuzzer() {
+    let q = `SELECT sstatus FROM mainsystem WHERE id = 101`;
+    dbConn.query(q,function(err, result){
+        if (err) throw err
+        else { 
+            let power = result[0]["sstatus"] == 1 ? "on":"off"
+            if (power === "on") {
+                let buzzerTopic = feedList.find(feed => feed.name === 'buzzer').link
+                // turn the buzzer on
+                adafruit.publish(buzzerTopic, {id: "3", name: "SPEAKER", data: "1000", unit: ""})
+                // after 1 second, turn the buzzer off
+                setTimeout(function () {
+                    adafruit.publish(buzzerTopic, {id: "3", name: "SPEAKER", data: "0", unit: ""});
+                }, 1000);}
+            }
+        }
+    )
+}
 
     //kiểm tra độ ẩm
-    module.exports = function checkHumid(message) {
-        let feed = feedList.find(feed => feed.name == "drv")
-        humid = Number(message.data)
+module.exports = function checkHumid(message) {
+    let feed = feedList.find(feed => feed.name == "drv")
+    let currentHumid = Number(message.data)
+    
+    let q1 = `SELECT upper_bound, lower_bound FROM farm.sensor WHERE id = '101'`
+    dbConn.query(q1, [true], function (err, result) {
+        if (err) throw err;
+        let {upper_bound: top, lower_bound: bottom} = result[0]
+        if (top && bottom) {
+            if (currentHumid < bottom) {
+                var mess = {
+                    id: "10",
+                    name: "DRV_PWM",
+                    data: 100 * 2.55,
+                    unit:""
+                } 
+            }
+            else {
+                var mess = {
+                    id: "10",
+                    name: "DRV_PWM",
+                    data: 0,
+                    unit:""
+                }
+            }
+        } else return res.status(400).send({ error: "Missing top or bottom value" })
         
-        let q1 = `SELECT upper_bound, lower_bound FROM farm.sensor WHERE id = '101'`
-        dbConn.query(q1, [true], function (err, result) {
-            if (err) throw err;
-            let top = result[0].upper_bound
-            let bottom = result[0].lower_bound
-            if (top && bottom) {
-                if (humid < bottom) {
-                    var mess = {
-                        id: "10",
-                        name: "DRV_PWM",
-                        data: 100 * 2.55,
-                        unit:""
-                    } 
-                }
-                else {
-                    var mess = {
-                        id: "10",
-                        name: "DRV_PWM",
-                        data: 0,
-                        unit:""
-                    }
-                }
-            } else return res.status(400).send({ error: "Missing top or bottom value" })
-            
-            let q2 = `SELECT sstatus FROM farm.mainsystem WHERE id = '101'`
-            dbConn.query(q2,function(err,result1){
-                if (err) res.json({error: err})
-                let power = result[0].sstatus
-                if (power == 1) { 
-                    adafruit.publish(feed.link, mess)
-                } else console.log("Power is off")
-            })
-        });      
-    }
+        let q2 = `SELECT sstatus FROM farm.mainsystem WHERE id = '101'`
+        dbConn.query(q2,function(err,result1){
+            if (err) res.json({error: err})
+            let power = result[0].sstatus
+            if (power == 1) { 
+                adafruit.publish(feed.link, mess)
+            } else console.log("Power is off")
+        })
+    });      
+}
     
